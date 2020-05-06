9f6f6a9297135199967a1abb160214d439277bc3
yoneyan <github@yoneyan.net>
2020-05-06 20:30:18 +0900

    現時点では未実装だが追加

---
+++ src/app/dashboard/usersetting/usersetting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usersetting',
+  templateUrl: './usersetting.component.html',
+  styleUrls: ['./usersetting.component.scss']
+})
+export class UsersettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
